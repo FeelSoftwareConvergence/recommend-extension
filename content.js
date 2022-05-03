6eafc4e466c3ef551c1245d865860ba09bf801dd
Tianea2160 <rhlehfndvkd7557@daum.net>
2022-05-03 22:00:42 +0900

    can ajax transmission and print ui

--- content.js
+++ content.js
@@ -1,6 +1,38 @@
-
 let test = document.getElementById("test")
 let newP = document.createElement("p")
-newP.textContent = "hello world";
 
-test.append(newP);
+let url = "http://localhost:8000";
+let method = "GET";
+
+// 통신을 위한 객체 생성
+let xhr = new XMLHttpRequest();
+
+// 상태변화를 알려주는 콜백함수
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === xhr.DONE) {
+        console.log(xhr.response);
+
+        // 실제 화면에 적용 response {"hello":"world"}
+        newP.textContent = xhr.response["hello"];
+        test.append(newP);
+    }
+}
+// 반환되는 타입을 명시해야함
+xhr.responseType = "json";
+// 비동기 적으로 처리
+xhr.open(method, url);
+xhr.send();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
